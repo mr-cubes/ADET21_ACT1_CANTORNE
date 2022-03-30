419dbf340e7afca73f5aafee8cbf0562cc7bac24
neilcantorne <cantorne.neil@gmail.com>
2022-03-30 18:12:05 +0800

    add LoginPage

---
+++ lib/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
