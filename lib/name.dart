dac4c98d5de32ace59b4126cfeb24f2aff2051ae
neilcantorne <cantorne.neil@gmail.com>
2022-03-30 21:37:46 +0800

    add Name class

---
+++ lib/name.dart
@@ -0,0 +1,8 @@
+class Name {
+  final String firstName;
+  final String middleName;
+  final String lastName;
+  final String extendedName;
+
+  Name(this.firstName, this.middleName, this.lastName, this.extendedName);
+}
