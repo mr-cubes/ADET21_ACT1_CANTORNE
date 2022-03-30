a9aad18ea13bba597d8485455591312902f2ca05
neilcantorne <cantorne.neil@gmail.com>
2022-03-30 19:06:30 +0800

    add login form

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,6 +11,28 @@ class LoginPage extends StatefulWidget {
 class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 40, 86, 212),
+        body: Center(
+            child: FractionallySizedBox(
+          widthFactor: 0.3,
+          heightFactor: 0.6,
+          child: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 232, 232, 232),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color.fromARGB(40, 0, 0, 0),
+                        spreadRadius: 4,
+                        blurRadius: 4,
+                        offset: Offset(4, 4))
+                  ]),
+              child: Form(
+                child: Column(
+                  children: [],
+                ),
+              )),
+        )));
   }
 }
