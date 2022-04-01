0aece552734b5f904eb6e6070a611047a0057b4c
neilcantorne <cantorne.neil@gmail.com>
2022-04-02 04:53:37 +0800

    add saveState

--- lib/bank_service.dart
+++ lib/bank_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:atm/account.dart';
 import 'package:atm/account_data.dart';
 import 'package:atm/card_number.dart';
@@ -5,6 +7,7 @@ import 'package:atm/name.dart';
 
 class BankService {
   final Map<int, AccountData> _map = Map();
+  final File _file = File("bank_data.json");
 
   static final BankService _instance = BankService._internal();
   BankService._internal();
@@ -34,5 +37,23 @@ class BankService {
     }
 
     _map[cardNumber.hashCode] = AccountData(pin, owner, initialBalance);
+  }
+
+  void saveState() {
+    var sink = _file.openWrite();
+
+    sink.writeln('{');
+
+    for (var entry in _map.entries) {
+      sink.write('"${entry.key}": ${entry.value.jsonString}');
+
+      if (entry.key != _map.entries.last.key) {
+        sink.writeln(',');
+      }
+    }
+
+    sink.writeln('}');
+
+    sink.close();
   }
 }
