f260b669dcddd9504298c34b06e2ea658263543c
neilcantorne <cantorne.neil@gmail.com>
2022-04-02 01:06:03 +0800

    remove LocalStorage

--- lib/account.dart
+++ lib/account.dart
@@ -1,9 +1,7 @@
-import 'package:localstorage/localstorage.dart';
 import 'package:atm/account_data.dart';
 
 class Account {
-  final LocalStorage _storage;
   final AccountData _accountData;
 
-  Account(this._storage, this._accountData);
+  Account(this._accountData);
 }
