317d3939b8b77d9d731239de269511a7c1952110
neilcantorne <cantorne.neil@gmail.com>
2022-04-02 04:14:15 +0800

    fix luhn algorithm

--- lib/card_number.dart
+++ lib/card_number.dart
@@ -3,30 +3,42 @@ class CardNumber {
 
   CardNumber(int value) {
     var sum = 0;
+    var weight = 0;
+    _value = value;
 
-    sum += value % 10;
+    // digit 0
+    weight = value % 10;
+    sum += weight;
     value = value ~/ 10;
 
-    sum += (value % 10) * 2;
+    // digit 1
+    weight = (value % 10) * 2;
+    sum += (weight % 10) + (weight ~/ 10);
     value = value ~/ 10;
 
-    sum += value % 10;
+    // digit 2
+    weight = value % 10;
+    sum += weight;
     value = value ~/ 10;
 
-    sum += (value % 10) * 2;
+    // digit 3
+    weight = (value % 10) * 2;
+    sum += (weight % 10) + (weight ~/ 10);
     value = value ~/ 10;
 
-    sum += value % 10;
+    // digit 4
+    weight = value % 10;
+    sum += weight;
     value = value ~/ 10;
 
-    sum += (value % 10) * 2;
+    // digit 5
+    weight = (value % 10) * 2;
+    sum += (weight % 10) + (weight ~/ 10);
     value = value ~/ 10;
 
     if (sum % 10 != 0) {
       throw Exception("Invalid credit card number");
     }
-
-    _value = value;
   }
 
   static CardNumber parse(String value) {
