3c835b979bf5d922e6e5a733775618a5223e2ad8
neilcantorne <cantorne.neil@gmail.com>
2022-04-04 21:11:34 +0800

    add middleInitial

--- lib/name.dart
+++ lib/name.dart
@@ -1,8 +1,30 @@
+import 'package:flutter/material.dart';
+
 class Name {
   final String firstName;
   final String middleName;
   final String lastName;
   final String extendedName;
+  late final String middleInitial;
 
-  Name(this.firstName, this.middleName, this.lastName, this.extendedName);
+  Name(this.firstName, this.middleName, this.lastName, this.extendedName) {
+    _setMiddleInitials();
+  }
+
+  void _setMiddleInitials() {
+    var buffer = StringBuffer();
+    var uppercase = middleName.toUpperCase();
+
+    for (var i = 0; i < middleName.length; i++) {
+      if (uppercase[i] == middleName[i]) {
+        buffer.write("${uppercase[i]}.");
+      }
+    }
+
+    middleInitial = buffer.toString();
+  }
+
+  String toProperName() {
+    return "$firstName $middleInitial $lastName";
+  }
 }
