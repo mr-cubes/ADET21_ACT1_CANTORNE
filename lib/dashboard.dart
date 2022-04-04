f634cde5479a783f7d75d4277d502c4c51c04482
neilcantorne <cantorne.neil@gmail.com>
2022-04-05 06:45:39 +0800

    add withdraw dialog

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:atm/account.dart';
+import 'package:atm/numerical_range_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -43,7 +46,9 @@ class DashboardState extends State<Dashboard> {
               child: ElevatedButton(
                 style: buttonStyle,
                 child: Text("Withdraw", style: buttonTextStyle),
-                onPressed: () {},
+                onPressed: () {
+                  showWithdrawDialog(context);
+                },
               )),
           Padding(
               padding: const EdgeInsets.all(12),
@@ -95,7 +100,10 @@ class DashboardState extends State<Dashboard> {
                                 padding: const EdgeInsets.all(12),
                                 child: ElevatedButton(
                                   child: const Text("Withdraw"),
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    showWithdrawDialog(context);
+                                  },
                                 )),
                             Padding(
                                 padding: const EdgeInsets.all(8),
@@ -108,6 +116,78 @@ class DashboardState extends State<Dashboard> {
                                 child: ElevatedButton(
                                   child: const Text("Go back"),
                                   onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ))
+                          ])
+                    ],
+                  )),
+            ),
+          );
+        });
+  }
+
+  void showWithdrawDialog(BuildContext context) {
+    var amountCountroller = TextEditingController(text: "200.00");
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)), //this right here
+            child: SizedBox(
+              width: 285,
+              height: 210,
+              child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                              "Balance PHP ${widget.account.getBalance().toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  color: Colors.black87, fontSize: 16),
+                              textAlign: TextAlign.center)),
+                      Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: SizedBox(
+                              width: 200,
+                              child: TextFormField(
+                                  controller: amountCountroller,
+                                  keyboardType: TextInputType.number,
+                                  textAlign: TextAlign.center,
+                                  inputFormatters: [
+                                    NumericalRangeFormatter(
+                                        200,
+                                        min(widget.account.getBalance(), 30000),
+                                        100)
+                                  ],
+                                  decoration: const InputDecoration(
+                                      label: Text("Amount",
+                                          textAlign: TextAlign.center),
+                                      hintText: "Enter amount")))),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: ElevatedButton(
+                                  child: const Text("Cancel"),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                )),
+                            Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: ElevatedButton(
+                                  child: const Text("OK"),
+                                  onPressed: () {
+                                    widget.account.withdraw(
+                                        double.parse(amountCountroller.text));
                                     Navigator.pop(context);
                                   },
                                 ))
