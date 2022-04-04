e77ac50789331959e75706f0f1292793626f9e30
neilcantorne <cantorne.neil@gmail.com>
2022-04-04 21:12:08 +0800

    add getOwner

--- lib/account.dart
+++ lib/account.dart
@@ -1,7 +1,10 @@
 import 'package:atm/account_data.dart';
+import 'package:atm/name.dart';
 
 class Account {
   final AccountData _accountData;
 
   Account(this._accountData);
+
+  Name getOwner() => _accountData.owner;
 }
