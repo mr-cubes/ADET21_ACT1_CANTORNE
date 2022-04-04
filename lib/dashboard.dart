d366332b05b54221e43c20a474db56df7a98f412
neilcantorne <cantorne.neil@gmail.com>
2022-04-04 21:12:29 +0800

    expand Dashboard appearance

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:atm/account.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Dashboard extends StatefulWidget {
   final Account account;
@@ -13,6 +14,51 @@ class Dashboard extends StatefulWidget {
 class DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Row(children: []));
+    var buttonStyle = ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.white70),
+        fixedSize: MaterialStateProperty.all<Size>(const Size(256, 38)));
+    var buttonTextStyle = const TextStyle(color: Colors.black87);
+
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 40, 86, 212),
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+              padding: const EdgeInsets.all(40),
+              child: Text(
+                  "Welcome ${widget.account.getOwner().toProperName()},\nwhat do you want to do?",
+                  style: const TextStyle(color: Colors.white, fontSize: 20))),
+          Padding(
+              padding: const EdgeInsets.all(12),
+              child: ElevatedButton(
+                style: buttonStyle,
+                child: Text("Check Balance", style: buttonTextStyle),
+                onPressed: () {},
+              )),
+          Padding(
+              padding: const EdgeInsets.all(12),
+              child: ElevatedButton(
+                style: buttonStyle,
+                child: Text("Withdraw", style: buttonTextStyle),
+                onPressed: () {},
+              )),
+          Padding(
+              padding: const EdgeInsets.all(12),
+              child: ElevatedButton(
+                style: buttonStyle,
+                child: Text("Deposit", style: buttonTextStyle),
+                onPressed: () {},
+              )),
+          Padding(
+              padding: const EdgeInsets.all(12),
+              child: ElevatedButton(
+                style: buttonStyle,
+                child: Text("Leave", style: buttonTextStyle),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ))
+        ])));
   }
 }
